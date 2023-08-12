6f689341686b7696e0a4804f6f5d4e29ad9bc9a5
tbohn2 <tanner.bohn@yahoo.com>
2023-08-11 22:26:39 -0700

    Added image and placed nav tabs

--- src/components/elements/header.js
+++ src/components/elements/header.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import image from '../images/ashley_logo3.png'
+import '../styles/header.css'
 
 const Header = () => {
     return (
-        <header className='header'>
-            <div className='header__logo'></div>
-            <nav className='header__nav'>
-                <Link to='/'>
-                    <button className='btn btn-primary'>Home</button>
-                </Link>
-                <Link to='/pricing'>
-                    <button className='btn btn-primary'>Home</button>
-                </Link>
-                <Link to='/aboutMe'>
-                    <button className='btn btn-primary'>Home</button>
-                </Link>
-                <Link to='/contact'>
-                    <button className='btn btn-primary'>Home</button>
-                </Link>
+        <header className='border border-light bg-light d-flex flex-column align-items-center justify-content-center'>
+            <div className='myImgBox border border-light bg-light d-flex flex-column align-items-center justify-content-center'>
+                <img className='myImg' src={image}></img>
+            </div>
+            <nav className='border border-light navbar d-flex justify-content-evenly col-12'>
+                <Link className='myLink nav-item fs-3 text-dark' to='/'>Home</Link>
+                <Link className='myLink nav-item fs-3 text-dark' to='/'>Pricing</Link>
+                <Link className='myLink nav-item fs-3 text-dark' to='/'>Contact</Link>
+                <Link className='myLink nav-item fs-3 text-dark' to='/'>About Me</Link>
+
             </nav>
         </header>
     )
-}
+};
 
 export default Header;
