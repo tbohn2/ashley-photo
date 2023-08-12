c6453e67e63d309132a36b1e705ecf89270d841b
tbohn2 <tanner.bohn@yahoo.com>
2023-08-11 22:27:06 -0700

    Applied styles to whole page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/elements/header';
+import './components/styles/root.css'
 
 function App() {
   return (
     <Router>
-      <div className=''>
-        <div className=''>
-          <Header />
+      <div className='bg-dark myBody flex flex-col'>
+        <Header />
+
+        {/* <div className=''>
+        <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/pricing' element={<Pricing />} />
+        <Route exact path='/contact' element={<Contact />} />
+        <Route exact path='/aboutMe/' element={<AboutMe />} />
+        </Routes>
         </div>
-        <div className=''>
-          <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route exact path='/pricing' element={<Pricing />} />
-            <Route exact path='/contact' element={<Contact />} />
-            <Route exact path='/aboutMe/' element={<AboutMe />} />
-          </Routes>
-        </div>
+        <div>
         <Footer />
+      </div> */}
       </div>
     </Router>
   );
