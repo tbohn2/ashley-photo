1ada94be78d645fb77e96d1bea51cfd248d88779
tbohn2 <tanner.bohn@yahoo.com>
2023-08-11 22:48:11 -0700

    Added buttons to nav and styled

--- src/components/elements/header.js
+++ src/components/elements/header.js
@@ -4,16 +4,34 @@ import image from '../images/ashley_logo3.png'
 import '../styles/header.css'
 
 const Header = () => {
+
+
     return (
-        <header className='border border-light bg-light d-flex flex-column align-items-center justify-content-center'>
+        <header className='bg-light d-flex flex-column align-items-center justify-content-center'>
             <div className='myImgBox border border-light bg-light d-flex flex-column align-items-center justify-content-center'>
                 <img className='myImg' src={image}></img>
             </div>
-            <nav className='border border-light navbar d-flex justify-content-evenly col-11'>
-                <Link className='myLink nav-item fs-3 text-dark' to='/'>Home</Link>
-                <Link className='myLink nav-item fs-3 text-dark' to='/'>Pricing</Link>
-                <Link className='myLink nav-item fs-3 text-dark' to='/'>Contact</Link>
-                <Link className='myLink nav-item fs-3 text-dark' to='/'>About Me</Link>
+            <nav className='d-flex justify-content-evenly align-items-end col-11 mt-1'>
+                <Link className='myLink fs-3 text-dark' to='/'>
+                    <button className='myBtn btn btn-dark fs-3'>
+                        Home
+                    </button>
+                </Link>
+                <Link className='myLink fs-3 text-dark' to='/'>
+                    <button className='myBtn btn btn-dark fs-3'>
+                        Pricing
+                    </button>
+                </Link>
+                <Link className='myLink fs-3 text-dark' to='/'>
+                    <button className='myBtn btn btn-dark fs-3'>
+                        Contact
+                    </button>
+                </Link>
+                <Link className='myLink fs-3 text-dark' to='/'>
+                    <button className='myBtn btn btn-dark fs-3'>
+                        About Me
+                    </button>
+                </Link>
 
             </nav>
         </header>
