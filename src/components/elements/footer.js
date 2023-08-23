6077bf66edbc4e0d3b9fad3869b29a1d1bd2a4fb
tbohn2 <tanner.bohn@yahoo.com>
2023-08-23 16:32:07 -0700

    Added icons to footer and styled

--- src/components/elements/footer.js
+++ src/components/elements/footer.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import FBicon from '../graphics/FBIcon.png'
+import IGicon from '../graphics/IGIcon.png'
+import pinIcon from '../graphics/PinIcon.png'
 import '../styles/footer.css'
 
 const Footer = () => {
@@ -8,20 +10,23 @@ const Footer = () => {
 
 
     return (
-        <footer className='d-flex mx-5'>
-            <div className='d-flex flex-column col-4 align-items-center border border-dark'>
-                <p>Ashley Bohn is a lifestyle family photographer based in Mesa, Arizona</p>
-                <h3>Follow Me</h3>
-                <Link to='https://www.instagram.com/ashleybohnphotography/'></Link>
-                <Link to='https://www.instagram.com/ashleybohnphotography/'></Link>
-            </div>
-            <div className='d-flex flex-column align-items-center col-4'>
-                <div className="footerLogo border border-dark"></div>
-            </div>
-            <div className='d-flex flex-column col-4 align-items-center border border-dark'>
-                <button className="bookBtn fs-3 my-3 btn btn-dark">Book Now</button>
-                <p>480-555-5555</p>
-                <p>ashleybohnphotography@gmail.com</p>
+        <footer className='d-flex justify-content-center col-12'>
+            <div className='d-flex col-10'>
+                <div className='d-flex flex-column col-5 align-items-center justify-content-evenly'>
+                    <p className='fs-3 col-8 mt-3'>Ashley Bohn is a lifestyle family photographer based in Mesa, Arizona</p>
+                    <button className="bookBtn fs-3 my-3 btn btn-dark">Book Now</button>
+                </div>
+                <div className='d-flex flex-column align-items-center col-2'>
+                    <div className="footerLogo"></div>
+                </div>
+                <div className='d-flex flex-column col-5 justify-content-evenly align-items-center'>
+                    <div className='d-flex justify-content-evenly col-8'>
+                        <Link className='mediaIcon fbIcon' to='https://www.instagram.com/ashleybohnphotography/'></Link>
+                        <Link className='mediaIcon igIcon' to='https://www.facebook.com/ashleybohnphotography/'></Link>
+                        <Link className='mediaIcon pinIcon' to='https://www.pinterest.com/ashleybohnphotography/'></Link>
+                    </div>
+                    <p className='fs-3'>ashleybohnphotography@gmail.com</p>
+                </div>
             </div>
         </footer>
     )
