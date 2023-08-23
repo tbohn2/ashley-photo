03fc3154202aac0451a955f22e3e29efc68be130
tbohn2 <tanner.bohn@yahoo.com>
2023-08-22 21:20:22 -0700

    Added sections to footer

--- src/components/elements/footer.js
+++ src/components/elements/footer.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import image from '../images/ashley_logo3.png'
-// import '../styles/footer.css'
+import logo from '../images/ashley-logo2.png'
+import '../styles/footer.css'
 
 const Footer = () => {
 
 
+
     return (
-        <footer className='footer d-flex justify-content-center border-top border-dark'>
-            <img src={image}></img>
+        <footer className='d-flex mx-5'>
+            <div className='d-flex flex-column col-4 align-items-center border border-dark'>
+                <p>Ashley Bohn is a lifestyle family photographer based in Mesa, Arizona</p>
+
+            </div>
+            <div className='d-flex flex-column align-items-center col-4'>
+                <div className="footerLogo border border-dark"></div>
+            </div>
+            <div></div>
         </footer>
     )
 };
