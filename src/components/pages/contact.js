25fc872ca940123b52e99a02271f6ecf17c52d7d
tbohn2 <tanner.bohn@yahoo.com>
2023-08-24 20:39:17 -0700

    Added to inputs to form

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -6,12 +6,20 @@ const Contact = () => {
     return (
         <div className="d-flex mt-3 flex-column align-items-center">
             <h1>CONTACT ME</h1>
-            <form className="d-flex flex-wrap col-10 border border-light p-2">
-                <div class="mb-3 col-2">
-                    <label class="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com" />
+            <form className="d-flex flex-column col-8 border border-light p-2 row g-1">
+                <div class="mb-3">
+                    <label class="form-label">Name</label>
+                    <input className="form-control" placeholder="Jane Doe" />
                 </div>
                 <div class="mb-3">
+                    <label class="form-label">PHONE NUMBER</label>
+                    <input className="form-control" placeholder="555-555-5555" />
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">EMAIL ADDRESS</label>
+                    <input type="email" className="form-control" placeholder="name@example.com" />
+                </div>
+                <div class="mb-3 col-4">
                     <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
                     <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                 </div>
