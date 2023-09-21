3f88b494dde31429927b28ae7c91adcdc0eeeb22
tbohn2 <tanner.bohn@yahoo.com>
2023-09-21 16:35:17 -0700

    Added button to book

--- src/components/pages/pricing.js
+++ src/components/pages/pricing.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
+import { Link } from 'react-router-dom';
 import '../styles/pricing.css'
 
 const Pricing = () => {
 
     return (
-        <div className="d-flex flex-column align-items-center">
-            <h1 className="fst-italic my-5">Packages</h1>
-            <div className="d-flex flex-wrap col-9">
+        <div className="d-flex flex-column my-5 align-items-center">
+            <h1 className="fst-italic">Packages</h1>
+            <div className="d-flex flex-wrap my-5 col-9">
                 <div className="d-flex flex-column align-items-center justify-content-between col-4 mb-3">
                     <h2 className="col-8 fw-bolder fs-1 text-center border-dark border-bottom">FAMILIES</h2>
                     <ul className="d-flex flex-column justify-content-start listIncluded list-unstyled fs-4 col-5">
@@ -68,7 +69,11 @@ const Pricing = () => {
                     <p className="fs-3">Investment: $100</p>
                 </div>
 
-
+            </div>
+            <div className="col-12 d-flex justify-content-center">
+                <Link className='fs-3 col-6 btn btn-light text-dark' to='/contact'>
+                    Book Now
+                </Link>
             </div>
         </div>
     )
