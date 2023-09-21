16d43a3c25bcafdd4a5297dac4e32ac8d408cdf2
tbohn2 <tanner.bohn@yahoo.com>
2023-09-20 22:03:16 -0700

    Finished making cards uniform

--- src/components/pages/pricing.js
+++ src/components/pages/pricing.js
@@ -17,77 +17,55 @@ const Pricing = () => {
                     <p className="fs-3">Investment: $100</p>
                 </div>
                 <div className="d-flex flex-column align-items-center justify-content-between col-4 mb-3">
-                    <h2 className="col-8 fw-bolder fs-1 text-center border-dark border-bottom">FAMILIES</h2>
+                    <h2 className="col-8 fw-bolder fs-1 text-center border-dark border-bottom">COUPLES</h2>
                     <ul className="d-flex flex-column justify-content-start listIncluded list-unstyled fs-4 col-5">
                         <li>1 location</li>
                         <li>1-2 hour session</li>
-                        <li>1-2 hour session</li>
-                        <li>60+ edited photos</li>
-                    </ul>
-                    <p className="fs-3">Investment: $100</p>
-                </div>
-                <div className="d-flex flex-column align-items-center justify-content-between col-4 mb-3">
-                    <h2 className="col-8 fw-bolder fs-1 text-center border-dark border-bottom">FAMILIES</h2>
-                    <ul className="d-flex flex-column justify-content-start list-unstyled fs-4 col-5">
-                        <li>1 location</li>
-                        <li>1-2 hour session</li>
-                        <li>1-2 hour session</li>
-                        <li>60+ edited photos</li>
-                    </ul>
-                    <p className="fs-3">Investment: $100</p>
-                </div>
-                <div className="d-flex flex-column align-items-center justify-content-between col-4 mb-3">
-                    <h2 className="col-8 fw-bolder fs-1 text-center border-dark border-bottom">COUPLES</h2>
-                    <h3>What is included?</h3>
-                    <ul className="list-unstyled fs-4">
-                        <li>1 location</li>
-                        <li>1-2 hour session</li>
                         <li>60+ edited photos</li>
                         <li>1 outfit change</li>
                     </ul>
                     <p className="fs-3">Investment: $100</p>
                 </div>
-                <div className="d-flex flex-column align-items-center col-4 mb-3">
+                <div className="d-flex flex-column align-items-center justify-content-between col-4 mb-3">
                     <h2 className="col-8 fw-bolder fs-1 text-center border-dark border-bottom">GRADUATION</h2>
-                    <h3>What is included?</h3>
-                    <ul className="list-unstyled fs-4">
+                    <ul className="d-flex flex-column justify-content-start listIncluded list-unstyled fs-4 col-5">
                         <li>1 location</li>
                         <li>1-2 hour session</li>
                         <li>60+ edited photos</li>
                         <li>1 outfit change</li>
-                        <li className="fs-3">Investment: $100</li>
                     </ul>
+                    <p className="fs-3">Investment: $100</p>
                 </div>
                 <div className="d-flex flex-column align-items-center col-4 mb-3">
                     <h2 className="col-8 fw-bolder fs-1 text-center border-dark border-bottom">MATERNITY</h2>
                     <h3>What is included?</h3>
-                    <ul className="list-unstyled fs-4">
+                    <ul className="d-flex flex-column justify-content-start listIncluded list-unstyled fs-4 col-5">
                         <li>1 location</li>
                         <li>1-2 hour session</li>
                         <li>60+ edited photos</li>
                         <li>1 outfit change</li>
-                        <li className="fs-3">Investment: $100</li>
                     </ul>
+                    <p className="fs-3">Investment: $100</p>
                 </div>
                 <div className="d-flex flex-column align-items-center col-4 mb-3">
                     <h2 className="col-8 fw-bolder fs-1 text-center border-dark border-bottom">HEADSHOTS</h2>
                     <h3>What is included?</h3>
-                    <ul className="list-unstyled fs-4">
+                    <ul className="d-flex flex-column justify-content-start listIncluded list-unstyled fs-4 col-5">
                         <li>1 location</li>
                         <li>1-2 hour session</li>
                         <li>60+ edited photos</li>
-                        <li className="fs-3">Investment: $100</li>
                     </ul>
+                    <p className="fs-3">Investment: $100</p>
                 </div>
                 <div className="d-flex flex-column align-items-center col-4 mb-3">
                     <h2 className="col-8 fw-bolder fs-1 text-center border-dark border-bottom">BUSINESS</h2>
                     <h3>What is included?</h3>
-                    <ul className="list-unstyled fs-4">
+                    <ul className="d-flex flex-column justify-content-start listIncluded list-unstyled fs-4 col-5">
                         <li>1 location</li>
                         <li>1-2 hour session</li>
                         <li>60+ edited photos</li>
-                        <li className="fs-3">Investment: $100</li>
                     </ul>
+                    <p className="fs-3">Investment: $100</p>
                 </div>
 
 
