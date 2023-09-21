ff4fb63e81305ef32995e9f517546818d4893847
tbohn2 <tanner.bohn@yahoo.com>
2023-09-20 21:54:16 -0700

    Made package cards uniform height

--- src/components/pages/pricing.js
+++ src/components/pages/pricing.js
@@ -7,11 +7,30 @@ const Pricing = () => {
         <div className="d-flex flex-column align-items-center">
             <h1 className="fst-italic my-5">Packages</h1>
             <div className="d-flex flex-wrap col-9">
-                <div className="d-flex flex-column align-items-center col-4 mb-3">
+                <div className="d-flex flex-column align-items-center justify-content-between col-4 mb-3">
                     <h2 className="col-8 fw-bolder fs-1 text-center border-dark border-bottom">FAMILIES</h2>
-                    <h3 className="">What is included?</h3>
-                    <ul className="list-unstyled fs-4">
+                    <ul className="d-flex flex-column justify-content-start listIncluded list-unstyled fs-4 col-5">
                         <li>1 location</li>
+                        <li>1-2 hour session</li>
+                        <li>60+ edited photos</li>
+                    </ul>
+                    <p className="fs-3">Investment: $100</p>
+                </div>
+                <div className="d-flex flex-column align-items-center justify-content-between col-4 mb-3">
+                    <h2 className="col-8 fw-bolder fs-1 text-center border-dark border-bottom">FAMILIES</h2>
+                    <ul className="d-flex flex-column justify-content-start listIncluded list-unstyled fs-4 col-5">
+                        <li>1 location</li>
+                        <li>1-2 hour session</li>
+                        <li>1-2 hour session</li>
+                        <li>60+ edited photos</li>
+                    </ul>
+                    <p className="fs-3">Investment: $100</p>
+                </div>
+                <div className="d-flex flex-column align-items-center justify-content-between col-4 mb-3">
+                    <h2 className="col-8 fw-bolder fs-1 text-center border-dark border-bottom">FAMILIES</h2>
+                    <ul className="d-flex flex-column justify-content-start list-unstyled fs-4 col-5">
+                        <li>1 location</li>
+                        <li>1-2 hour session</li>
                         <li>1-2 hour session</li>
                         <li>60+ edited photos</li>
                     </ul>
