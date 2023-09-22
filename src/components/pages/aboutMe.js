fb37123d79305545f215595ec7846a891929b812
tbohn2 <tanner.bohn@yahoo.com>
2023-09-21 17:32:46 -0700

    Added portrait and about me paragraph

--- src/components/pages/aboutMe.js
+++ src/components/pages/aboutMe.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
-import '../styles/home.css'
+import '../styles/aboutMe.css'
+import pic from '../images/aboutMePortrait.jpg'
 
 const AboutMe = () => {
 
     return (
         <div className="d-flex mt-5 flex-column align-items-center">
-
+            <h1 className="ist-italic mt-5">MEET YOUR PHOTOGRAPHER</h1>
+            <div className="col-7 my-5 border aboutMeContainer">
+                <div className="float-end  border aboutMePortrait" style={{ backgroundImage: `url(${pic})` }}></div>
+                <p className="fs-1">Hello! My name is Ashley! I am passionate about photography.
+                    I have been doing it for years and am a pro dawg. Hello! My name is Ashley! I am passionate about photography. I have been doing it for years and am a pro dawg
+                    Hello! My name is Ashley! I am passionate about photography. I have been doing it for years and am a pro dawg
+                    Hello! My name is Ashley! I am passionate about photography. I have been doing it for years and am a pro dawg</p>
+            </div>
         </div>
     )
 }
