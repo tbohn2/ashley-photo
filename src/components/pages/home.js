c2bd5c10434e310510d4769d062c6300ddd24b92
tbohn2 <tanner.bohn@yahoo.com>
2023-09-24 19:33:28 -0700

    Changed style of final break

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from 'react-router-dom';
 import '../styles/home.css'
 import pic1 from '../images/homepage1.jpg'
 import pic2 from '../images/homepage2.jpg'
@@ -21,14 +22,14 @@ const Home = () => {
                     <img className="homepageImg" src={pic3}></img>
                 </div>
             </div>
-            <div className="welcomeBreak border-top border-bottom border-dark gloock fst-italic col-12 myLgText d-flex justify-content-center align-items-center">Welcome to Ashley Bohn Photography!</div>
-            <div className="col-12 d-flex justify-content-center">
+            <div className="welcomeBreak  gloock fst-italic col-12 myLgText d-flex justify-content-center align-items-center">Welcome to Ashley Bohn Photography!</div>
+            <div className="col-12 my-5 d-flex justify-content-center">
                 <div className="d-flex flex-column col-8">
                     <div className="d-flex justify-content-evenly align-items-center">
                         <div className="myPortrait z-0 col-3 d-flex" style={{ backgroundImage: `url(${ashleyPic})` }}>
                             {/* <div className="myGraphic" style={{ backgroundImage: `url(${flower1})` }}></div> */}
                         </div>
-                        <div className="d-flex flex-column align-items-center col-7">
+                        <div className="d-flex flex-column align-items-center col-5">
                             <h1 className="meetHeader col-6 text-center">Meet Ashley</h1>
                             <p className="p-1 myInfoText">One time I ate a hot pocket, and it was so delicious that I ran back to Walmart to buy another package (because I ate the whole package). When
                                 I got to Walmart, I sprinted down the aisle and grabbed as many hotpocket boxes as my little arms could hold. As I sprinted to self-checkout, suddenly I felt the urge to share with everyone and bought everyone some hot pockets. Yum!</p>
@@ -36,9 +37,18 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-            <div className="d-flex justify-content-evenly align-items-center my-5 py-4 col-12 bg-secondary">
-                <h1 className="fs-2">Professional Photo for your lifestyle</h1>
-                <button className="bookBtn fs-2 btn btn-dark col-2">Book Now</button>
+            <div className="d-flex justify-content-evenly align-items-center py-4 col-12 bg-secondary">
+                <div className="d-flex justify-content-evenly align-items-center col-10 bg-secondary">
+                    <div className="d-flex align-items-center bModa">
+                        <h1 className="myLgText fw-bold d-flex align-items-center">Professional Photo for your</h1>
+                        <h1 className="myLgText fw-bold fst-italic">&nbsp;lifestyle</h1>
+                    </div>
+                    <Link className='col-2' to='/contact'>
+                        <button className='bookBtn fs-2 btn btn-dark col-12'>
+                            Book Now
+                        </button>
+                    </Link>
+                </div>
             </div>
         </div>
     )
