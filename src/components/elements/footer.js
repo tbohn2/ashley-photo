4452d44d597dd0ade87614adbaf262e912e70cd0
tbohn2 <tanner.bohn@yahoo.com>
2023-09-24 18:58:05 -0700

    Made the footer logo dark

--- src/components/elements/footer.js
+++ src/components/elements/footer.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import FBicon from '../graphics/FBIcon.png'
 import IGicon from '../graphics/IGIcon.png'
 import pinIcon from '../graphics/PinIcon.png'
+import logo from '../graphics/logo.png'
 import '../styles/footer.css'
 
 const Footer = () => {
@@ -17,7 +18,7 @@ const Footer = () => {
                     <button className="bookBtn fs-3 my-3 btn btn-dark">Book Now</button>
                 </div>
                 <div className='d-flex flex-column align-items-center col-2'>
-                    <div className="footerLogo"></div>
+                    <div className="footerLogo" style={{ backgroundImage: `url(${logo})` }}></div>
                 </div>
                 <div className='d-flex flex-column col-5 justify-content-evenly align-items-center'>
                     <div className='d-flex justify-content-evenly col-8'>
