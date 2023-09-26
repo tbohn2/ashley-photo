107c5e0891bf84fbbfcbedfcf26a4e705685197d
tbohn2 <tanner.bohn@yahoo.com>
2023-09-25 20:03:24 -0700

    Added collage images to aboutMe page

--- src/components/pages/aboutMe.js
+++ src/components/pages/aboutMe.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import '../styles/aboutMe.css'
 import pic from '../images/aboutMePortrait.jpg'
+import collage1 from '../images/collage1.png'
+import collage2 from '../images/collage2.png'
+import collage3 from '../images/collage3.png'
 
 const AboutMe = () => {
 
@@ -13,6 +16,11 @@ const AboutMe = () => {
                     I have been doing it for years and am a pro dawg. Hello! My name is Ashley! I am passionate about photography. I have been doing it for years and am a pro dawg
                     Hello! My name is Ashley! I am passionate about photography. I have been doing it for years and am a pro dawg
                     Hello! My name is Ashley! I am passionate about photography. I have been doing it for years and am a pro dawg</p>
+            </div>
+            <div className="col-10 my-5 d-flex justify-content-evenly">
+                <img className="col-3 collage" src={collage2} />
+                <img className="col-3 border border-dark collage" src={collage1} />
+                <img className="col-3 collage" src={collage3} />
             </div>
         </div>
     )
