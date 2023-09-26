f1ab783cf51512145579b84f50410a67ecd255dd
tbohn2 <tanner.bohn@yahoo.com>
2023-09-25 20:49:52 -0700

    Added bg; made cards uniform; styled title

--- src/components/pages/pricing.js
+++ src/components/pages/pricing.js
@@ -1,35 +1,28 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import '../styles/pricing.css'
+import pricingBG from '../images/pricingBG.jpg'
 
 const Pricing = () => {
 
     return (
-        <div className="d-flex flex-column my-5 align-items-center">
-            <h1 className="fst-italic">Packages</h1>
-            <div className="d-flex flex-wrap my-5 col-9">
-                <div className="d-flex flex-column align-items-center justify-content-between col-4 mb-3">
-                    <h2 className="col-8 fw-bolder fs-1 text-center border-dark border-bottom">FAMILIES</h2>
-                    <ul className="d-flex flex-column justify-content-start listIncluded list-unstyled fs-4 col-5">
+        <div className="d-flex flex-column mt-5 align-items-center pricingBG" style={{ backgroundImage: `url(${pricingBG})` }}>
+            <div className="d-flex align-items-center gloock packageHeader bgBrown align-self-start myLgText col-6 pe-5 text-end">
+                <h1 className="col-10 my-1">P a c k a g e s</h1>
+            </div>
+            <div className="d-flex flex-wrap justify-content-evenly my-5 col-9">
+                <div className="priceCard d-flex flex-column align-items-center bgTeal justify-content-between col-3 py-3 my-5 mx-3">
+                    <h2 className="col-9 fw-bolder fs-1 text-center border-dark border-bottom">FAMILIES</h2>
+                    <ul className="d-flex flex-column justify-content-start listIncluded list-unstyled fs-4 col-6">
                         <li>1 location</li>
                         <li>1-2 hour session</li>
                         <li>60+ edited photos</li>
                     </ul>
                     <p className="fs-3">Investment: $100</p>
                 </div>
-                <div className="d-flex flex-column align-items-center justify-content-between col-4 mb-3">
-                    <h2 className="col-8 fw-bolder fs-1 text-center border-dark border-bottom">COUPLES</h2>
-                    <ul className="d-flex flex-column justify-content-start listIncluded list-unstyled fs-4 col-5">
-                        <li>1 location</li>
-                        <li>1-2 hour session</li>
-                        <li>60+ edited photos</li>
-                        <li>1 outfit change</li>
-                    </ul>
-                    <p className="fs-3">Investment: $100</p>
-                </div>
-                <div className="d-flex flex-column align-items-center justify-content-between col-4 mb-3">
-                    <h2 className="col-8 fw-bolder fs-1 text-center border-dark border-bottom">GRADUATION</h2>
-                    <ul className="d-flex flex-column justify-content-start listIncluded list-unstyled fs-4 col-5">
+                <div className="priceCard d-flex flex-column align-items-center bgTeal justify-content-between col-3 py-3 my-5 mx-3">
+                    <h2 className="col-9 fw-bolder fs-1 text-center border-dark border-bottom">COUPLES</h2>
+                    <ul className="d-flex flex-column justify-content-start listIncluded list-unstyled fs-4 col-6">
                         <li>1 location</li>
                         <li>1-2 hour session</li>
                         <li>60+ edited photos</li>
@@ -37,10 +30,9 @@ const Pricing = () => {
                     </ul>
                     <p className="fs-3">Investment: $100</p>
                 </div>
-                <div className="d-flex flex-column align-items-center col-4 mb-3">
-                    <h2 className="col-8 fw-bolder fs-1 text-center border-dark border-bottom">MATERNITY</h2>
-                    <h3>What is included?</h3>
-                    <ul className="d-flex flex-column justify-content-start listIncluded list-unstyled fs-4 col-5">
+                <div className="priceCard d-flex flex-column align-items-center bgTeal justify-content-between col-3 py-3 my-5 mx-3">
+                    <h2 className="col-9 fw-bolder fs-1 text-center border-dark border-bottom">GRADUATION</h2>
+                    <ul className="d-flex flex-column justify-content-start listIncluded list-unstyled fs-4 col-6">
                         <li>1 location</li>
                         <li>1-2 hour session</li>
                         <li>60+ edited photos</li>
@@ -48,20 +40,31 @@ const Pricing = () => {
                     </ul>
                     <p className="fs-3">Investment: $100</p>
                 </div>
-                <div className="d-flex flex-column align-items-center col-4 mb-3">
-                    <h2 className="col-8 fw-bolder fs-1 text-center border-dark border-bottom">HEADSHOTS</h2>
+                <div className="priceCard d-flex flex-column align-items-center bgTeal justify-content-between col-3 py-3 my-5 mx-3">
+                    <h2 className="col-9 fw-bolder fs-1 text-center border-dark border-bottom">MATERNITY</h2>
                     <h3>What is included?</h3>
-                    <ul className="d-flex flex-column justify-content-start listIncluded list-unstyled fs-4 col-5">
+                    <ul className="d-flex flex-column justify-content-start listIncluded list-unstyled fs-4 col-6">
+                        <li>1 location</li>
+                        <li>1-2 hour session</li>
+                        <li>60+ edited photos</li>
+                        <li>1 outfit change</li>
+                    </ul>
+                    <p className="fs-3">Investment: $100</p>
+                </div>
+                <div className="priceCard d-flex flex-column align-items-center bgTeal justify-content-between col-3 py-3 my-5 mx-3">
+                    <h2 className="col-9 fw-bolder fs-1 text-center border-dark border-bottom">HEADSHOTS</h2>
+                    <h3>What is included?</h3>
+                    <ul className="d-flex flex-column justify-content-start listIncluded list-unstyled fs-4 col-6">
                         <li>1 location</li>
                         <li>1-2 hour session</li>
                         <li>60+ edited photos</li>
                     </ul>
                     <p className="fs-3">Investment: $100</p>
                 </div>
-                <div className="d-flex flex-column align-items-center col-4 mb-3">
-                    <h2 className="col-8 fw-bolder fs-1 text-center border-dark border-bottom">BUSINESS</h2>
+                <div className="priceCard d-flex flex-column align-items-center bgTeal justify-content-between col-3 py-3 my-5 mx-3">
+                    <h2 className="col-9 fw-bolder fs-1 text-center border-dark border-bottom">BUSINESS</h2>
                     <h3>What is included?</h3>
-                    <ul className="d-flex flex-column justify-content-start listIncluded list-unstyled fs-4 col-5">
+                    <ul className="d-flex flex-column justify-content-start listIncluded list-unstyled fs-4 col-6">
                         <li>1 location</li>
                         <li>1-2 hour session</li>
                         <li>60+ edited photos</li>
