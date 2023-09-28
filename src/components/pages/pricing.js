c9ab5cb1a9994c94d77f309cd2ae52e435dd0e08
tbohn2 <tanner.bohn@yahoo.com>
2023-09-27 21:29:07 -0700

    Changed bg color of header; styled submit button

--- src/components/pages/pricing.js
+++ src/components/pages/pricing.js
@@ -6,11 +6,11 @@ import pricingBG from '../images/pricingBG.jpg'
 const Pricing = () => {
 
     return (
-        <div className="d-flex flex-column mt-5 align-items-center pricingBG" style={{ backgroundImage: `url(${pricingBG})` }}>
-            <div className="d-flex align-items-center gloock packageHeader bgBrown align-self-start myLgText col-6 pe-5 text-end">
+        <div className="d-flex flex-column mt-5 pb-3 align-items-center pricingBG" style={{ backgroundImage: `url(${pricingBG})` }}>
+            <div className="d-flex align-items-center gloock bgGray packageHeader align-self-start myLgText col-6 pe-5 text-end">
                 <h1 className="col-10 my-1">P a c k a g e s</h1>
             </div>
-            <div className="d-flex flex-wrap justify-content-evenly my-5 col-9">
+            <div className="d-flex flex-wrap justify-content-evenly mt-5 col-9">
                 <div className="priceCard d-flex flex-column align-items-center bgTeal justify-content-between col-3 py-3 my-5 mx-3">
                     <h2 className="col-9 fw-bolder fs-1 text-center border-dark border-bottom">FAMILIES</h2>
                     <ul className="d-flex flex-column justify-content-start listIncluded list-unstyled fs-4 col-6">
@@ -71,9 +71,8 @@ const Pricing = () => {
                     </ul>
                     <p className="fs-3">Investment: $100</p>
                 </div>
-
             </div>
-            <div className="col-12 d-flex justify-content-center">
+            <div className="col-12 pb-5 d-flex justify-content-center">
                 <Link className='fs-3 col-6 btn btn-light text-dark' to='/contact'>
                     Book Now
                 </Link>
