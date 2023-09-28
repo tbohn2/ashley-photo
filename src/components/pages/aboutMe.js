2c905e017542cd621f1f6222b8322b2ffd2293fe
tbohn2 <tanner.bohn@yahoo.com>
2023-09-27 20:55:38 -0700

    Changed font of page header

--- src/components/pages/aboutMe.js
+++ src/components/pages/aboutMe.js
@@ -8,9 +8,12 @@ import collage3 from '../images/collage3.png'
 const AboutMe = () => {
 
     return (
-        <div className="d-flex mt-5 flex-column align-items-center">
-            <h1 className="ist-italic mt-5">MEET YOUR PHOTOGRAPHER</h1>
-            <div className="col-7 my-5 border">
+        <div className="d-flex pt-5 bgTeal flex-column align-items-center">
+            <div className="d-flex align-items-center">
+                <h1 className="gloock d-flex align-items-center">MEET YOUR</h1>
+                <h1 className="myXLText allura">&nbsp;Photographer</h1>
+            </div>
+            <div className="col-7 my-5">
                 <div className="float-end  border aboutMePortrait" style={{ backgroundImage: `url(${pic})` }}></div>
                 <p className="fs-1">Hello! My name is Ashley! I am passionate about photography.
                     I have been doing it for years and am a pro dawg. Hello! My name is Ashley! I am passionate about photography. I have been doing it for years and am a pro dawg
