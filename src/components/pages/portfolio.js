91c098936d3778a5cc7e8812491b9026c49ebdac
tbohn2 <tanner.bohn@yahoo.com>
2023-10-02 19:57:49 -0700

    Added pictures to portfolio page

--- src/components/pages/portfolio.js
+++ src/components/pages/portfolio.js
@@ -5,19 +5,34 @@ import portfolio2 from '../images/portfolio2.jpg'
 import portfolio3 from '../images/portfolio3.jpg'
 import portfolio4 from '../images/portfolio4.jpg'
 import portfolio5 from '../images/portfolio5.jpg'
+import portfolio6 from '../images/portfolio6.jpg'
+import portfolio7 from '../images/portfolio7.jpg'
+import portfolio8 from '../images/portfolio8.jpg'
+import portfolio9 from '../images/portfolio9.jpg'
+import portfolio10 from '../images/portfolio10.jpg'
+import portfolio11 from '../images/portfolio11.jpg'
+import portfolio12 from '../images/portfolio12.jpg'
 
 const Portfolio = () => {
 
     return (
         <div className="fade-in d-flex mt-5 justify-content-center">
-            <div className="col-10 d-flex flex-wrap">
+            <div className="col-10 d-flex flex-wrap justify-content-center">
                 <div className="portPortrait d-flex flex-column justify-content-between col-3 mx-5 my-3">
                     <img className="portLandscape" src={portfolio1} alt="portfolio1"></img>
                     <img className="portLandscape" src={portfolio4} alt="portfolio4"></img>
                 </div>
                 <img className="portPortrait col-3 mx-5 my-3" src={portfolio2} alt="portfolio2"></img>
                 <img className="portPortrait col-3 mx-5 my-3" src={portfolio5} alt="portfolio5"></img>
-                <img className="portPortrait col-3 mx-5 my-3" src={portfolio3} alt="portfolio3"></img>
+                <img className="portPortrait col-3 mx-5 my-3" src={portfolio6} alt="portfolio6"></img>
+                <img className="portPortrait col-3 mx-5 my-3" src={portfolio7} alt="portfolio7"></img>
+                <img className="portPortrait col-3 mx-5 my-3" src={portfolio8} alt="portfolio8"></img>
+                <img className="portPortrait col-3 mx-5 my-3" src={portfolio9} alt="portfolio9"></img>
+                <img className="portPortrait col-3 mx-5 my-3" src={portfolio10} alt="portfolio10"></img>
+                <div className="portPortrait d-flex flex-column justify-content-between col-3 mx-5 my-3">
+                    <img className="portLandscape" src={portfolio11} alt="portfolio11"></img>
+                    <img className="portLandscape" src={portfolio12} alt="portfolio12"></img>
+                </div>
             </div>
 
         </div>
