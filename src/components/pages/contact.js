442f3955eccb392aa71174d158ce8c0316bb6b39
tbohn2 <tanner.bohn@yahoo.com>
2023-10-04 15:11:25 -0700

    Added sendEmail function

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,7 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "../styles/contact.css"
+import emailjs from '@emailjs/browser';
+
 
 const Contact = () => {
+
+    // const [formState, setFormState] = useState({
+    //     name: '',
+    //     phone: '',
+    //     email: '',
+    //     instagram: '',
+    //     inquiryType: '',
+    //     message: '',
+    // });
+
+    // const handleChange = (event) => {
+    //     const { name, value } = event.target;
+    //     setFormState({
+    //         ...formState,
+    //         [name]: value,
+    //     });
+    // };
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
 
     const [selectedOption, setSelectedOption] = useState("What type of photo session would you like to book with me?");
 
@@ -16,26 +48,26 @@ const Contact = () => {
                 <h3 className="ms-5">Please complete the form below</h3>
                 <h3 className="me-5 align-self-end text-decoration-underline">I look forward to connecting with you!</h3>
             </div>
-            <form className="fs-5 d-flex flex-column col-6 border border-light p-2 row g-1">
+            <form ref={form} onSubmit={sendEmail} className="fs-5 d-flex flex-column col-6 border border-light p-2 row g-1">
                 <div className="mb-3">
                     <label className="form-label">NAME</label>
-                    <input className="form-control" placeholder="Jane Doe" />
+                    <input name="name" className="form-control" placeholder="Jane Doe" />
                 </div>
                 <div className="mb-3">
                     <label className="form-label">PHONE NUMBER</label>
-                    <input className="form-control" placeholder="555-555-5555" />
+                    <input name="phone" className="form-control" placeholder="555-555-5555" />
                 </div>
                 <div className="mb-3">
                     <label className="form-label">EMAIL ADDRESS</label>
-                    <input type="email" className="form-control" placeholder="name@example.com" />
+                    <input name="email" type="email" className="form-control" placeholder="name@example.com" />
                 </div>
                 <div className="mb-3">
                     <label className="form-label">INSTAGRAM HANDLE</label>
-                    <input type="email" className="form-control" placeholder="optional" />
+                    <input name="instagram" className="form-control" placeholder="optional" />
                 </div>
                 <div className="col-12 mb-3">
                     <label className="form-label">TYPE OF INQUIRY</label>
-                    <button type="button" id="selectedOption" className="col-12 btn btn-light text-start dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    <button name="inquiryType" type="button" id="selectedOption" className="col-12 btn btn-light text-start dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                         {selectedOption}
                     </button>
                     <ul className="dropdown-menu dropdown-menu-start">
@@ -48,7 +80,7 @@ const Contact = () => {
                 </div>
                 <div className="mb-3 col-12">
                     <label className="form-label">YOUR INSPIRATION</label>
-                    <textarea className="form-control" placeholder="Tell me all the fun details! Your inspiration for the shoot, date, desired location, ideas, etc." rows="3"></textarea>
+                    <textarea name="message" className="form-control" placeholder="Tell me all the fun details! Your inspiration for the shoot, date, desired location, ideas, etc." rows="3"></textarea>
                 </div>
                 <button className="submitBtn col-2 align-self-start">Submit</button>
             </form>
