21acba58e098502ab7f7adced2525d3389564259
tbohn2 <tanner.bohn@yahoo.com>
2023-10-09 20:40:45 -0700

    Added queryselectors for inputs & phone validation

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -5,34 +5,32 @@ import emailjs from '@emailjs/browser';
 
 const Contact = () => {
 
-    // const [formState, setFormState] = useState({
-    //     name: '',
-    //     phone: '',
-    //     email: '',
-    //     instagram: '',
-    //     inquiryType: '',
-    //     message: '',
-    // });
-
-    // const handleChange = (event) => {
-    //     const { name, value } = event.target;
-    //     setFormState({
-    //         ...formState,
-    //         [name]: value,
-    //     });
-    // };
-
     const form = useRef();
 
-    const sendEmail = (e) => {
+    const sendForm = (e) => {
         e.preventDefault();
 
-        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
+        const nameInput = form.current.querySelector('input[name="name"]').value;
+        const phoneInput = form.current.querySelector('input[name="phone"]').value;
+        const emailInput = form.current.querySelector('input[name="email"]').value;
+        const instagramInput = form.current.querySelector('input[name="instagram"]').value;
+        const inquiryTypeInput = form.current.querySelector('button[name="inquiryType"]').value;
+        const messageInput = form.current.querySelector('textarea[name="message"]').value;
+
+        console.log(nameInput, phoneInput, emailInput, instagramInput, inquiryTypeInput, messageInput);
+
+        if (!/^\d{3}-\d{3}-\d{4}$/.test(phoneInput)) {
+            alert("Please enter a valid phone number in the format '555-555-5555'.");
+            e.preventDefault();
+            return;
+        }
+
+        // emailjs.sendForm('service_dzeb4os', 'template_nnq7g1f', form.current, 'YOUR_PUBLIC_KEY')
+        //     .then((result) => {
+        //         console.log(result.text);
+        //     }, (error) => {
+        //         console.log(error.text);
+        //     });
     };
 
     const [selectedOption, setSelectedOption] = useState("What type of photo session would you like to book with me?");
@@ -48,7 +46,7 @@ const Contact = () => {
                 <h3 className="ms-5">Please complete the form below</h3>
                 <h3 className="me-5 align-self-end text-decoration-underline">I look forward to connecting with you!</h3>
             </div>
-            <form ref={form} onSubmit={sendEmail} className="fs-5 d-flex flex-column col-xxl-6 col-8 border border-light p-2 row g-1">
+            <form ref={form} onSubmit={sendForm} className="fs-5 d-flex flex-column col-xxl-6 col-8 border border-light p-2 row g-1">
                 <div className="mb-3">
                     <label className="form-label">NAME</label>
                     <input name="name" className="form-control" placeholder="Jane Doe" />
@@ -59,7 +57,7 @@ const Contact = () => {
                 </div>
                 <div className="mb-3">
                     <label className="form-label">EMAIL ADDRESS</label>
-                    <input name="email" type="email" className="form-control" placeholder="name@example.com" />
+                    <input name="email" type="email" id="email" className="form-control" placeholder="name@example.com" />
                 </div>
                 <div className="mb-3">
                     <label className="form-label">INSTAGRAM HANDLE</label>
