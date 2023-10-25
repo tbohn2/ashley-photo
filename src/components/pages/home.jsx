1975a85dbc19f891bf6bb20f4477b5f01b06e70e
tbohn2 <tanner.bohn@yahoo.com>
2023-10-24 19:41:28 -0700

    Changed spacing on homepage

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -128,87 +128,85 @@ const Home = () => {
                         </div>
                     )}
                 </div>
-            )
-                :
-                (
-                    <div className="d-flex justify-content-center col-12" >
-                        {imagesLoaded ? (
-                            <div className="d-flex flex-column align-items-center col-12">
-                                <div className="homeGallery col-10 d-flex justify-content-evenly align-items-center">
-                                    <div className="d-flex justify-content-center align-items-center mx-3 homepageImgContainer">
-                                        <img className="homepageImg" src={pic1}></img>
-                                    </div>
-                                    <div className="d-flex justify-content-center align-items-center mx-3 homepageImgContainer">
-                                        <img className="homepageImg2 mb-5 pb-5" src={pic2}></img>
-                                    </div>
-                                    <div className="d-flex justify-content-center align-items-center mx-3 homepageImgContainer">
-                                        <img className="homepageImg" src={pic3}></img>
-                                    </div>
+            ) : (
+                <div className="d-flex justify-content-center col-12" >
+                    {imagesLoaded ? (
+                        <div className="d-flex flex-column align-items-center col-12">
+                            <div className="homeGallery col-10 d-flex justify-content-evenly align-items-center">
+                                <div className="d-flex justify-content-center align-items-center mx-3 homepageImgContainer">
+                                    <img className="homepageImg" src={pic1}></img>
                                 </div>
-                                <div className="bgTeal col-12 d-flex flex-column justify-content-center align-items-center">
-                                    <p className="allura fs-2 welcome1">Welcome to Ashley Bohn Photography!</p>
-                                    <p className="gloock myLgText">I'D LOVE TO HEAR FROM YOU</p>
-                                    <p className="fs-5 welcome3 mb-2">For session availability please fill out this <Link className="formLink" to={'/contact'}>form</Link> and I will get back to you.</p>
-                                    <img src={graphic5}></img>
+                                <div className="d-flex justify-content-center align-items-center mx-3 homepageImgContainer">
+                                    <img className="homepageImg2 mb-5 pb-5" src={pic2}></img>
                                 </div>
-                                <div className="meetSection col-12 py-5 d-flex justify-content-center">
-                                    <div className="d-flex flex-column col-10">
-                                        <div className="d-flex justify-content-evenly align-items-end">
-                                            <div className="myPortrait z-1 col-4 col-xl-3 d-flex" style={{ backgroundImage: `url(${ashleyPic})` }}></div>
-                                            <div className="d-flex flex-column align-self-center z-1 align-items-center col-7 col-xl-6">
-                                                <h1 className="meetHeader col-10 text-center">Hi, I'm Ashley!</h1>
-                                                <div className="border border-dark myTextBorder1">
-                                                    <div className="border border-dark myTextBorder2 d-flex justify-content-center align-items-center">
-                                                        <p className="bgWhite myInfoText p-3 col-11 d-flex flex-column justify-content-center align-items-center">
-                                                            I am the girl behind the lens! I love capturing sweet and special moments with my camera. You can find me exploring AZ, planning my next vacay, or getting cozy at home with my hubby or a book in my hand.
-                                                            I am excited to get to know you!
-                                                            <br></br>
-                                                            <Link className='col-6 col-sm-8 my-5 align-self-center' to='/aboutMe'>
-                                                                <button className='fs-4 col-12 moreAbout' onClick={scrollToTop}>
-                                                                    More about me!
-                                                                </button>
-                                                            </Link>
-                                                        </p>
-                                                    </div>
+                                <div className="d-flex justify-content-center align-items-center mx-3 homepageImgContainer">
+                                    <img className="homepageImg" src={pic3}></img>
+                                </div>
+                            </div>
+                            <div className="bgTeal col-12 d-flex flex-column justify-content-center align-items-center">
+                                <p className="allura fs-2 welcome1">Welcome to Ashley Bohn Photography!</p>
+                                <p className="gloock myLgText">I'D LOVE TO HEAR FROM YOU</p>
+                                <p className="fs-5 welcome3 mb-2">For session availability please fill out this <Link className="formLink" to={'/contact'}>form</Link> and I will get back to you.</p>
+                                <img src={graphic5}></img>
+                            </div>
+                            <div className="meetSection col-12 py-5 d-flex justify-content-center">
+                                <div className="d-flex flex-column col-10">
+                                    <div className="d-flex justify-content-evenly align-items-end">
+                                        <div className="myPortrait z-1 col-4 col-xl-3 d-flex" style={{ backgroundImage: `url(${ashleyPic})` }}></div>
+                                        <div className="d-flex flex-column align-self-center z-1 align-items-center col-7 col-xl-6">
+                                            <h1 className="meetHeader col-10 text-center">Hi, I'm Ashley!</h1>
+                                            <div className="border border-dark myTextBorder1">
+                                                <div className="border border-dark myTextBorder2 d-flex justify-content-center align-items-center">
+                                                    <p className="bgWhite myInfoText p-3 col-11 d-flex flex-column justify-content-center align-items-center">
+                                                        I am the girl behind the lens! I love capturing sweet and special moments with my camera. You can find me exploring AZ, planning my next vacay, or getting cozy at home with my hubby or a book in my hand.
+                                                        I am excited to get to know you!
+                                                        <br></br>
+                                                        <Link className='col-6 col-sm-8 my-5 align-self-center' to='/aboutMe'>
+                                                            <button className='fs-4 col-12 moreAbout' onClick={scrollToTop}>
+                                                                More about me!
+                                                            </button>
+                                                        </Link>
+                                                    </p>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <div className='graphic3-container graphic3-1 z-1'>
-                                        <img className="graphic3" src={graphic3} alt='graphic3'></img>
-                                    </div>
-                                    <div className='graphic3-container graphic3-2'>
-                                        <img className="graphic3" src={graphic3} alt='graphic3'></img>
-                                    </div>
-                                    <div className='graphic3-container graphic3-3 z-0'>
-                                        <img className="graphic3" src={graphic3} alt='graphic3'></img>
-                                    </div>
-                                    <div className='graphic3-container graphic3-4'>
-                                        <img className="graphic3" src={graphic3} alt='graphic3'></img>
-                                    </div>
                                 </div>
-                                <div className="bgLightTeal d-flex justify-content-evenly align-items-center mt-5 py-5 col-12">
-                                    <div className="d-flex justify-content-evenly align-items-center col-12">
-                                        <div className="d-flex align-items-center bModa">
-                                            <h1 className="fs-1 fw-bold d-flex align-items-center">Professional Photo for your</h1>
-                                            <h1 className="fs-1 fw-bold fst-italic">&nbsp;lifestyle</h1>
-                                        </div>
-                                        <Link className='col-3' to='/contact'>
-                                            <button className='fs-1 bookBtn col-12' onClick={scrollToTop}>
-                                                Book Now
-                                            </button>
-                                        </Link>
-                                    </div>
+                                <div className='graphic3-container graphic3-1 z-1'>
+                                    <img className="graphic3" src={graphic3} alt='graphic3'></img>
+                                </div>
+                                <div className='graphic3-container graphic3-2'>
+                                    <img className="graphic3" src={graphic3} alt='graphic3'></img>
+                                </div>
+                                <div className='graphic3-container graphic3-3 z-0'>
+                                    <img className="graphic3" src={graphic3} alt='graphic3'></img>
+                                </div>
+                                <div className='graphic3-container graphic3-4'>
+                                    <img className="graphic3" src={graphic3} alt='graphic3'></img>
                                 </div>
                             </div>
-                        ) : (
-                            <div className="homeGallery col-12 my-5 d-flex flex-column align-items-center">
-                                <h1 className="myXLText allura">Welcome!</h1>
-                                <h2 className="myLgText allura">Loading...</h2>
+                            <div className="bgLightTeal d-flex justify-content-evenly align-items-center mt-5 py-5 col-12">
+                                <div className="d-flex justify-content-evenly align-items-center col-12">
+                                    <div className="d-flex align-items-center bModa">
+                                        <h1 className="fs-1 fw-bold d-flex align-items-center">Professional Photo for your</h1>
+                                        <h1 className="fs-1 fw-bold fst-italic">&nbsp;lifestyle</h1>
+                                    </div>
+                                    <Link className='col-3' to='/contact'>
+                                        <button className='fs-1 bookBtn col-12' onClick={scrollToTop}>
+                                            Book Now
+                                        </button>
+                                    </Link>
+                                </div>
                             </div>
-                        )}
-                    </div>
-                )
+                        </div>
+                    ) : (
+                        <div className="homeGallery col-12 my-5 d-flex flex-column align-items-center">
+                            <h1 className="myXLText allura">Welcome!</h1>
+                            <h2 className="myLgText allura">Loading...</h2>
+                        </div>
+                    )}
+                </div>
+            )
             }
         </div>
     )
